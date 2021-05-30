ee1c5e8f3e54529d42faea320ff63a1eb8bc0e75
2000DED <44520488+2000DED@users.noreply.github.com>
2021-05-30 10:14:09 +0300

    Update createdMas.ino

--- createdMas.ino
+++ createdMas.ino
@@ -1,18 +1,5 @@
-#define CUBE_HIGHT_B 41
-#define CUBE_HIGHT_S 31
-
-#define I_TO_3 byte i = 0; i <= 2; i++
-#define J_TO_3 byte j = 0; j <= 2; j++
-
-#define FOR_I_5 byte i = 0; i <= 4; i++
-#define FOR_J_5 byte j = 0; j <= 4; j++
-
-
-byte count = 0;
-byte massPlanKyd[5][2];
-byte finalMas[20][7];
-byte countStep = 0;
-byte spiersMas[4];
+#include "GyverStepper.h"
+#include <Servo.h>
 
 //1 - красный
 //2 - синий
@@ -20,29 +7,88 @@ byte spiersMas[4];
 //4 - зелёный
 //5 - black
 
-byte inputMas[5][2] = {{3, 6}, {4, 9}, {5, 8}, {1, 10}, {2, 7}};
-byte outputMas[3][3] = {{4, 1, 2}, {5, 9, 8}, {7, 0, 0}};
+//шпиль 1
+//этаж 0
+//зона материалов 0
+//зона постройки 1
+//output массив типа (тип, зона откуда, местоположение в зоне, высота в мм, зона куда, местоположение в зоне, высота в мм)
 
-void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  lord_of_the_builders_two_arrays(inputMas, outputMas);
-  for (byte i = 0; i <= 19; i++) {
-    for (byte j = 0; j <= 6; j++) {
-      Serial.print(finalMas[i][j]);
-      Serial.print(" ");
-    }
-    Serial.println();
-  }
-  Serial.println("-----------------------------");
-}
 
-void loop() {
+//---------------CONST LOGIC---------------
 
-}
+#define CUBE_HIGHT_B 41
+#define CUBE_HIGHT_S 31
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+byte count = 0;
+byte finalMas[20][7] = {  {1, 0, 0, 40, 1, 1, 40},
+  {1, 0, 1, 40, 0, 2, 40},
+  {99, 0, 0, 0, 0, 0, 0}
+};
+byte countStep = 2;
+byte spiersMas[4];
 
+byte inputMas[5][2] = {{1, 8}, {3, 10}, {2, 7}, {4, 6}, {5, 9}};
+byte outputMas[3][3] = {{8, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+//---------------CONST MANIPULATOR---------------
+
+#define STEPPER_EN_PIN 8
+// The X Stepper pins
+#define STEPPER1_DIR_PIN 5  ////////change pins!!!!!
+#define STEPPER1_STEP_PIN 2
+// The Y stepper pins
+#define STEPPER2_DIR_PIN 6
+#define STEPPER2_STEP_PIN 3
+
+#define A_AXIS_LIMIT_SWITCH_PIN 9
+#define Z_AXIS_LIMIT_SWITCH_PIN 10
+
+#define SERVO_X   12
+#define SERVO_Y   7
+#define SERVO_Z   4
+#define SERVO_ZZ  13
+
+#define CUBE_HEIGHT_B 41
+#define CUBE_HEIGHT_S 31
+
+GStepper<STEPPER2WIRE> Astepper(200, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
+GStepper<STEPPER2WIRE> Zstepper(200, STEPPER2_STEP_PIN, STEPPER2_DIR_PIN, STEPPER_EN_PIN);
+
+Servo ServoX;
+Servo ServoY;
+Servo ServoZ;
+Servo ServoZZ;
+
+int ServX[4] = {135, 110, 60, 50};
+int ServY[3] = {130, 90, 23};
+int ServZ[3] = {80, 101, 138};
+int ServZZ[2] = {70, 30};
+const int gap = 5;
+const int turnPerBigTurn = 8;
+const int stepInmm = 5;
+
+const int stepPerTurn = 200;
+const int AAxisTurn = stepPerTurn * 8;
+const int ZAxisTurn = stepPerTurn * 8;
+
+int Stepper[4] = {0, 0, 0 , 0};
+
+int InputArrPlan[3][4] = { { 1, 2, 0, 0 }, { 2, 4, 3, 0 }, { 5, 4, 0, 0 } };  ///////     X  Z
+
+int InputArrFack[5][2] = { { 1, 2 }, { 2, 3 }, { 4, 3 }, { 5, 0 }, { 2, 4 } };  ///////     X  Z
+/*
+  int MasOfAction[4][7] = { {1, 1, 0, 40, 1, 2, 40},
+                            {0, 0, 1, 70, 1, 0, 40},
+                            {1, 1, 0, 40, 0, 1, 70},
+                            {0, 0, 1, 70, 1, 0, 40}
+  };         ///////     (type, PlaceIn, xIn, yIn, PlaceOut, xOut, yOut)
+*/
+int MasOfActionSteps[80][5];                            ///////     (Astepper, Zstepper, S1, S2, grab)
+
+int IndexOfActionSteps = 0;
+int MainIndex = 0;
+
+//---------------END CONST---------------
 
 void search_spiers(byte outputMas[3][3]) {
   byte countSpiers = 100;
@@ -68,10 +114,8 @@ void search_spiers(byte outputMas[3][3]) {
       }
     }
   }
-
   spiersMas[3] = indexDom;
 }
-
 
 
 void array_generator(byte type, byte plaseA, byte numInPlaseA, byte HightInPlaseA, byte plaseB, byte numInPlaseB, byte HightInPlaseB) {
@@ -85,18 +129,33 @@ void array_generator(byte type, byte plaseA, byte numInPlaseA, byte HightInPlase
   countStep++;
 }
 
-byte lord_of_the_builders_brotherhood_array()
-{
+
+void build_matrix(int oneF, int twoF, int threeF) {
+  outputMas[0][0] = byte(oneF / 100);
+  outputMas[0][1] = byte(oneF / 10 % 10);
+  outputMas[0][2] = byte(oneF % 10);
+  outputMas[1][0] = byte(twoF / 100);
+  outputMas[1][1] = byte(twoF / 10 % 10);
+  outputMas[1][2] = byte(twoF % 10);
+  outputMas[2][0] = byte(threeF / 100);
+  outputMas[2][1] = byte(threeF / 10 % 10);
+  outputMas[2][2] = byte(threeF % 10);
+  //PlatfornIO IDE
+}
+
+
+byte lord_of_the_builders_brotherhood_array() { //153221534
   Serial.println("Go");
   while (true) {
     if (Serial.available() > 0) {
       String data = Serial.readStringUntil('\n');
       long longdata = data.toInt();
+      Serial.println(longdata);
       if (longdata > 100) {
-        int floorOne = int(longdata - (longdata % 1000) / 1000000);
-        int floorTwo = int((longdata - (longdata % 1000) / 1000) - (floorOne * 1000));
-        int floorThree = int(longdata % 1000000);
-        byte massPlanPos = build_matrix(floorOne, floorTwo, floorThree);
+        int floorOne = int(longdata / 1000000);
+        int floorTwo = int(longdata / 1000 % 1000);
+        int floorThree = int(longdata % 1000);
+        build_matrix(floorOne, floorTwo, floorThree);
       }
       else {
         byte typeE = byte(longdata % 10);
@@ -104,24 +163,24 @@ byte lord_of_the_builders_brotherhood_array()
         count++;
         switch (count) {
           case 1:
-            massPlanKyd[0][0] = typeE;
-            massPlanKyd[0][1] = typeW + 5;
+            inputMas[0][0] = typeE;
+            inputMas[0][1] = typeW + 5;
             break;
           case 2:
-            massPlanKyd[1][0] = typeE;
-            massPlanKyd[1][1] = typeW + 5;
+            inputMas[1][0] = typeE;
+            inputMas[1][1] = typeW + 5;
             break;
           case 3:
-            massPlanKyd[2][0] = typeE;
-            massPlanKyd[2][1] = typeW + 5;
+            inputMas[2][0] = typeE;
+            inputMas[2][1] = typeW + 5;
             break;
           case 4:
-            massPlanKyd[3][0] = typeE;
-            massPlanKyd[3][1] = typeW + 5;
+            inputMas[3][0] = typeE;
+            inputMas[3][1] = typeW + 5;
             break;
           case 5:
-            massPlanKyd[4][0] = typeE;
-            massPlanKyd[4][1] = typeW + 5;
+            inputMas[4][0] = typeE;
+            inputMas[4][1] = typeW + 5;
             break;
         }
       }
@@ -129,31 +188,6 @@ byte lord_of_the_builders_brotherhood_array()
   }
 }
 
-byte build_matrix(int oneF, int twoF, int threeF) {
-  byte massPlanPos[3][3];
-  massPlanPos[0][0] = byte(oneF - (oneF % 100) / 100);
-  massPlanPos[0][1] = byte(oneF - (oneF % 10) / 100);
-  massPlanPos[0][2] = byte(oneF - ((oneF - (oneF % 100)) - (oneF % 10)) / 10);
-  massPlanPos[1][0] = byte(twoF - (twoF % 100) / 100);
-  massPlanPos[1][1] = byte(twoF - ((twoF - (twoF % 100)) - (twoF % 10)) / 10);
-  massPlanPos[1][2] = byte(twoF % 10);
-  massPlanPos[2][0] = byte(threeF - (threeF % 100) / 100);
-  massPlanPos[2][1] = byte(threeF - ((threeF - (threeF % 100)) - (threeF % 10)) / 10);
-  massPlanPos[2][2] = byte(threeF % 10);
-  return massPlanPos;
-  //PlatfornIO IDE
-}
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//шпиль 1
-//этаж 0
-//зона материалов 0
-//зона постройки 1
-//input массив откуда, массив куда
-//output массив типа (тип, зона откуда, местоположение в зоне, высота в мм, зона куда, местоположение в зоне, высота в мм)
 
 byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) {
 
@@ -165,7 +199,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
 
 
-  for (I_TO_3) {
+  for (byte i = 0; i <= 2; i++) {
     if (outputMas[0][i] > 5) countInOneFloor++;
   }
 
@@ -194,7 +228,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
     byte oneFloorMas[2] = {11, 11};
     byte countOneFloor = 0;
-    for (I_TO_3) {
+    for (byte i = 0; i <= 2; i++) {
       /*Serial.print("outputMas[0][i] - ");
         Serial.println(outputMas[0][i]);
         Serial.print("spiersMas[3] - ");
@@ -216,7 +250,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
     byte stopMas[2] = {0, 0};
 
-    for (FOR_I_5) {
+    for (byte i = 0; i <= 4; i++) {
       for (byte j = 0; j <= 1; j++) {
         if (inputMas[i][0] == oneFloorMas[j]) {
           /*Serial.print("i - ");
@@ -233,7 +267,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
     for (byte j = 0; j <= 2; j++) {
       if (outputMas[0][j] < 6 and outputMas[0][j] != 0 and j != spiersMas[3]) {
-        for (FOR_I_5) {
+        for (byte i = 0; i <= 4; i++) {
           flagErrorVar2 = false;
           /*Serial.print("outputMas[0][j] - ");
             Serial.println(outputMas[0][j]);
@@ -289,7 +323,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
     byte countSS = 0;
 
 
-    for (FOR_I_5) {
+    for (byte i = 0; i <= 4; i++) {
       if (inputMas[i][1] != 0) {
         stupidSpiers[countSS] = i;
         countSS++;
@@ -301,7 +335,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
     bool flag = false;
 
-    for (I_TO_3) {
+    for (byte i = 0; i <= 2; i++) {
       if (outputMas[2][i] > 0) flag = true;
     }
 
@@ -311,7 +345,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
       countSS = 0;
       byte flagSS = 0;
 
-      for (I_TO_3) {
+      for (byte i = 0; i <= 2; i++) {
         //Serial.print("outputMas[1][i] - ");
         //Serial.println(outputMas[1][i]);
         if (outputMas[1][i] < 6 and outputMas[1][i] > 0) {
@@ -323,7 +357,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
       }
 
       if (flagSS == 2) {
-        for (FOR_I_5) {
+        for (byte i = 0; i <= 4; i++) {
           for (byte j = 0; j <= 1; j++) {
             //Serial.print("stupidSpiersNum2[j][0] - ");
             //Serial.println(stupidSpiersNum2[j][0]);
@@ -367,7 +401,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
         bool errorFlag = true;
 
-        for (I_TO_3) {
+        for (byte i = 0; i <= 2; i++) {
           if (outputMas[i][spiersMas[3]] < 6 and outputMas[i][spiersMas[3]] != 0) {
             errorFlag = false;
           }
@@ -379,7 +413,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
         else {
           array_generator(1, 1, spiersMas[3], 0, 1, saveAss, 2 * CUBE_HIGHT_B + CUBE_HIGHT_S);
 
-          for (FOR_I_5) {
+          for (byte i = 0; i <= 4; i++) {
             if (outputMas[spiersMas[3]][0] == inputMas[i][0]) {
               array_generator(0, 0, i, 0, 1, spiersMas[3], 0);
             }
@@ -392,7 +426,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
       }
 
       else if (flagSS == 1) {
-        for (FOR_I_5) {
+        for (byte i = 0; i <= 4; i++) {
           for (byte j = 0; j <= 1; j++) {
             if (stupidSpiersNum2[j][0] == inputMas[i][0]) {
               array_generator(0, 0, i, CUBE_HIGHT_B, 1, stupidSpiersNum2[j][1], CUBE_HIGHT_B);
@@ -403,18 +437,18 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
         byte countSpirtsTwoFloor = 0;
 
-        for (I_TO_3)
+        for (byte i = 0; i <= 2; i++)
           if (outputMas[1][i] > 5 and outputMas[1][i] <= 10) countSpirtsTwoFloor++;
 
         if (countSpirtsTwoFloor == 2) {
           byte spirtsMas[2][2];
-          for (I_TO_3) {
+          for (byte i = 0; i <= 2; i++) {
             if (outputMas[2][i] > 5 and outputMas[2][i] <= 10 and i != spiersMas[3]) {
               spirtsMas[0][0] = i;
               spirtsMas[0][1] = outputMas[2][i];
             }
           }
-          for (I_TO_3) {
+          for (byte i = 0; i <= 2; i++) {
             if (outputMas[1][i] > 5 and outputMas[1][i] <= 10 and i != spiersMas[3]) {
               spirtsMas[1][0] = i;
               spirtsMas[1][1] = outputMas[1][i];
@@ -424,8 +458,8 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
           byte countDa = 0;
           byte saveAssVar2;
 
-          for (I_TO_3) {
-            for (J_TO_3) {
+          for (byte i = 0; i <= 2; i++) {
+            for (byte j = 0; j <= 2; j++) {
               for (int k = 0; k <= 1; k++) {
                 if (outputMas[i][j] == spirtsMas[k][1]) {
                   if (countDa == 0) {
@@ -442,7 +476,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
             }
           }
           array_generator(1, 1, spiersMas[3], 0, 1, saveAssVar2, CUBE_HIGHT_B + CUBE_HIGHT_S);
-          for (FOR_I_5) {
+          for (byte i = 0; i <= 4; i++) {
             if (inputMas[i][0] == outputMas[0][spiersMas[3]])
               array_generator(0, 0, i, 0, 1, spiersMas[3], 0);
           }
@@ -452,13 +486,13 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
         else if (countSpirtsTwoFloor == 1) {
           byte spirtsMas[2][2];
-          for (I_TO_3) {
+          for (byte i = 0; i <= 2; i++) {
             if (outputMas[2][i] > 5 and outputMas[2][i] <= 10 and i != spiersMas[3]) {
               spirtsMas[0][0] = i;
               spirtsMas[0][1] = outputMas[2][i];
             }
           }
-          for (I_TO_3) {
+          for (byte i = 0; i <= 2; i++) {
             if (outputMas[1][i] > 5 and outputMas[1][i] <= 10 and i != spiersMas[3]) {
               spirtsMas[1][0] = i;
               spirtsMas[1][1] = outputMas[1][i];
@@ -468,8 +502,8 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
           byte countDa = 0;
           byte saveAssVar2;
 
-          for (I_TO_3) {
-            for (J_TO_3) {
+          for (byte i = 0; i <= 2; i++) {
+            for (byte j = 0; j <= 2; j++) {
               for (byte k = 0; k <= 1; k++) {
                 if (outputMas[i][j] == spirtsMas[k][1]) {
                   if (countDa == 0) {
@@ -508,7 +542,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
       byte errorVar = 0;
       byte saveAssVar3;
       for (int i = 2; i >= 0; i--) {
-        for (J_TO_3) {
+        for (byte j = 0; j <= 2; j++) {
           if (failSimulation[i][spiersMas[3]] == outputMas[1][j]) {
             if (errorVar == 0) {
               array_generator(1, 1, spiersMas[3], 2 * CUBE_HIGHT_S, 1, j, CUBE_HIGHT_B);
@@ -527,12 +561,12 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
       bool flagUh = false;
 
-      for (I_TO_3) {
+      for (byte i = 0; i <= 2; i++) {
         if (outputMas[i][spiersMas[3]] > 0 and outputMas[i][spiersMas[3]] < 6) flagUh = true;
       }
       if (flagUh) {
         array_generator(1, 1, spiersMas[3], 0, 1, saveAssVar3, CUBE_HIGHT_S + CUBE_HIGHT_B);
-        for (FOR_I_5) {
+        for (byte i = 0; i <= 4; i++) {
           if (outputMas[0][spiersMas[3]] == inputMas[i][0])
             array_generator(0, 0, i, 0, 1, spiersMas[3], 0);
         }
@@ -545,8 +579,8 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
   }
   else {
     if (countInOneFloor == 3) {
-      for (I_TO_3) {
-        for (FOR_J_5) {
+      for (byte i = 0; i <= 2; i++) {
+        for (byte j = 0; j <= 4; j++) {
           if (outputMas[0][i] == inputMas[j][1]) {
             array_generator(1, 0, j, CUBE_HIGHT_B, 1, i, 0);
           }
@@ -555,21 +589,21 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
     }
     else {
       byte errorIndex;
-      for (I_TO_3) {
+      for (byte i = 0; i <= 2; i++) {
         if (outputMas[0][i] < 6 and outputMas[0][i] > 0) errorIndex = i;
       }
       byte countFloorMeh = 0;
       byte oneKub[2] = {0 , 0};
       byte twoKub[2] = {0, 0};
-      for (I_TO_3) {
+      for (byte i = 0; i <= 2; i++) {
         if (outputMas[i][errorIndex] < 6 and outputMas[i][errorIndex] > 0) {
           oneKub[countFloorMeh] = outputMas[i][errorIndex];
           countFloorMeh++;
         }
       }
 
-      for (I_TO_3) {
-        for (FOR_J_5) {
+      for (byte i = 0; i <= 2; i++) {
+        for (byte j = 0; j <= 4; j++) {
           if (outputMas[0][i] == inputMas[j][1]) {
             array_generator(1, 0, j, CUBE_HIGHT_B, 1, i, 0);
             inputMas[j][1] = 0;
@@ -577,7 +611,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
         }
       }
 
-      for (FOR_I_5) {
+      for (byte i = 0; i <= 4; i++) {
         for (byte j = 0; j <= 1; j++) {
           if (inputMas[i][0] == oneKub[j] and inputMas[i][1] != 0 ) {
             for (byte k = 0; k <= 4 ; k++) {
@@ -594,7 +628,7 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
 
       byte countMinusVara = 0;
 
-      for (FOR_I_5) {
+      for (byte i = 0; i <= 4; i++) {
         for (byte j = 0; j <= 1; j++) {
           if (inputMas[i][0] == oneKub[j]) {
             if (countMinusVara == 0) {
@@ -608,8 +642,8 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
           }
         }
       }
-      for (I_TO_3) {
-        for (FOR_J_5) {
+      for (byte i = 0; i <= 2; i++) {
+        for (byte j = 0; j <= 4; j++) {
           if (countMinusVara == 1) {
             if (outputMas[1][i] == inputMas[j][1] and outputMas[1][i] != 0) {
               array_generator(1, 0, j, CUBE_HIGHT_B, 1, i, CUBE_HIGHT_B);
@@ -626,3 +660,35 @@ byte lord_of_the_builders_two_arrays(byte inputMas[5][2], byte outputMas[3][3]) 
   }
   return 1;
 }
+
+
+
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  delay(50000);
+  lord_of_the_builders_brotherhood_array();
+  lord_of_the_builders_brotherhood_array();
+  lord_of_the_builders_brotherhood_array();
+  lord_of_the_builders_brotherhood_array();
+  lord_of_the_builders_brotherhood_array();
+  lord_of_the_builders_brotherhood_array();
+
+  lord_of_the_builders_two_arrays(inputMas, outputMas);
+
+}
+
+void loop() {
+
+}
+
+//---------------FUNCTION LOGIC---------------
+
+
+
+//---------------FUNCTION MANIPULATOR---------------
+
+//---------------END PROGGRAM---------------
