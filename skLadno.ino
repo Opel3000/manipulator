9bf02a37cd9e00f9dc7bafc70fc75c8d32a36cdd
Opelcun4uk <59770189+Alekcandr220@users.noreply.github.com>
2021-06-28 20:09:39 +0300

    Update skLadno.ino

--- skLadno.ino
+++ skLadno.ino
@@ -1,28 +1,56 @@
-void skLadno(int oneStepA, int oneSpetZ, int twoStepA, int twoStepZ) {
+int maxSpeedA = 1500;
+int maxSpeedZ = 1200;
 
-  //maxSpeedA
-  //maxSpeedZ
+int speedA = 0;
+int speedZ = 0;
 
-  //speedA
-  //speedZ
+
+void setup() {
+  Serial.begin(9600);
+  skLadno(-400, 0, 635, 800);
+  Serial.println(speedA);
+  Serial.println(speedZ);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void skLadno(int oneStepA, int oneStepZ, int twoStepA, int twoStepZ) {
+
+  //int maxSpeedA = 1500;
+  //int maxSpeedZ = 1200;
+
+  //int speedA = 0;
+  //int speedZ = 0;
+
 
   int raznostA = twoStepA - oneStepA;
   int raznostZ = twoStepZ - oneStepZ;
   int maxSpeedAZ = 0;
+
 
   if (raznostA < 0)
     raznostA *= -1;
   if (raznostZ < 0)
     raznostZ *= -1;
 
-  if (raznostA >= raznostZ) {
-    float timeAZ = raznostA/maxSpeedA;
-    speedZ = raznostZ/timeAZ;
+  if (raznostA > raznostZ) {
+    speedZ = raznostZ / (float(raznostA) / maxSpeedA);
     speedA = maxSpeedA;
+    if (speedZ > maxSpeedZ) {
+      speedA = maxSpeedZ;
+      speedZ = maxSpeedZ;
+    }
+  }
+  else if (raznostA == raznostZ) {
+    speedA = maxSpeedZ;
+    speedZ = maxSpeedZ;
   }
   else {
-    float timeAZ = raznostZ/ = maxSpeedZ;
-    speedA = raznostA/timeAZ;
+    speedA = raznostA / (float(raznostZ) / maxSpeedZ);
     speedZ = maxSpeedZ;
   }
 }
