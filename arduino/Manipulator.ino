4b3f71dabb9266a509cd5456c5d27d93a856d092
Opelcun4uk <59770189+Opel3000@users.noreply.github.com>
2021-07-02 15:45:49 +0300

    Update Manipulator.ino

--- arduino/Manipulator.ino
+++ arduino/Manipulator.ino
@@ -365,7 +365,7 @@ void build_matrix(int oneF, int twoF, int threeF) {
 
 
 byte lord_of_the_builders_brotherhood_array() { //153221534
-  Serial.println("Go");
+  Serial.print("g");
   while (true) {
     if (Serial.available() > 0) {
       String data = Serial.readStringUntil('\n');
@@ -553,7 +553,7 @@ void lord_of_the_builders_two_arrays() {
         for (byte k = 0; k <= 2; k++) {
           for (byte j = 0; j <= 4; j++) {
             if (inputMas[j][1] == spiersMas[k]) {
-              array_generator(1, 0, j, CUBE_HIGHT_B, 0, spiersMas[3], CUBE_HIGHT_B + (countSpiersForInputMasTrash * CUBE_HIGHT_S));
+              array_generator(1, 0, j, CUBE_HIGHT_B, 1, spiersMas[3], CUBE_HIGHT_B + (countSpiersForInputMasTrash * CUBE_HIGHT_S));
               if (countSpiersInMainZone != 2) {
                 spiersInMainZone[countSpiersInMainZone] = inputMas[j][1];
               }
